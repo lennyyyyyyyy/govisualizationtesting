0eae0f3ef34d48f0e5ab857ae653fc04711b0346
Devin Lim <devinlim2025@gmail.com>
2024-06-17 14:24:23 -0400

    add sente and start of pyscript

---
+++ game.py
@@ -0,0 +1,7 @@
+import sente
+from pyscript import window, document
+from pyscript.js_modules import jsgame
+from pyweb import pydom
+
+game = sente.Game()
+jsgame.setup_board()
---
+++ index.mjs
@@ -0,0 +1,24 @@
+function setup_board() {
+    var board = new WGo.Board(document.getElementById("board"), {
+        width: 600,
+    });
+    return board;
+}
+
+function clear_board(board) {
+    board.removeAllObjects();
+}
+
+function overlay_position(board, position) {
+    for (let i=0; i<19; i++) {
+        for (let j=0; j<19; j++) {
+            if (position[i][j] == "BLACK") {
+                board.addObject({x: i, y: j, c: Wgo.B});
+            } else if (position[i][j] == "WHITE") {
+                board.addObject({x: i, y: j, c: Wgo.W});
+            }
+        }
+    }
+}
+
+export { setup_board, clear_board, overlay_position, overlay_statistics }
